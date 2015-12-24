ba3bfe9b425c5552f079532efb0e58b8072afa6a
miage-thibaut <thibaut.rousselle7@gmail.com>
2015-12-24 08:39:51 +0100

    corrige la partie "Entrepôt"

--- Entrepot.py
+++ Entrepot.py
@@ -2,31 +2,31 @@
 # -*- coding: utf8 -*-
 import os
 import sys
-from tkinter import * 
+# import nécessaire à la création de l'interface graphique
+import tkinter
 
-class Entrepot: #CrÃ©ation de l'objet EntrepÃ´t
+class Entrepot: #Création de l'objet Entrepôt
 
-    # Contruction de l'EntrepÃ´t, qui comporte une taille maximale, et 3 listes de piÃ¨ces qui ont Ã©tÃ© traitÃ©es
-    # Respectivement notÃ©es piecesA, piecesB et piecesC. 
-    def __init__(self, tailleMax): # Construit un entrepÃ´t
+    # Contruction de l'Entrepôt, qui comporte une taille maximale.
+    def __init__(self, tailleMax): # Construit un entrepôt
         self.tailleMax = tailleMax
 
     def increaseA(self):
-        # Fonction servant Ã  incrÃ©menter la valeur du compteur A afin que sa valeur puisse Ãªtre mise Ã  jour pour l'ensemble de la procÃ©dure.
+        # Fonction servant à incrémenter la valeur du compteur A afin que sa valeur puisse être mise à jour pour l'ensemble de la procédure.
         global comptA # Python recherche la valeur du compteur en dehors de l'espace local de la fonction.
         comptA += 1
 
     def increaseB(self):
-        # Fonction servant Ã  incrÃ©menter la valeur du compteur B.
+        # Fonction servant à incrémenter la valeur du compteur B.
         global comptB
         comptB += 1
 
     def increaseC(self):
-        # Fonction servant Ã  incrÃ©menter la valeur du compteur C.
+        # Fonction servant à incrémenter la valeur du compteur C.
         global comptC
         comptC += 1
 
-    # MÃ©thode qui rÃ©cupÃ¨re les piÃ¨ces traitÃ©es par les machines (qui ont Ã©tÃ© modifiÃ©es en rA/rB/rC prÃ©cedemment), et incrÃ©mente le compteur liÃ©
+    # Méthode qui récupère les pièces traitées par les machines (qui ont été modifiées en résultats de type rA/rB/rC précedemment), et incrémente le compteur lié
     def recuperationPiece(self,Piece,compteurA,compteurB,compteurC):
         if Piece.type == 'rA':
             entrepot.increaseA()
@@ -35,28 +35,28 @@ class Entrepot: #CrÃ©ation de l'objet EntrepÃ´t
         elif Piece.type == 'rC':
             entrepot.increaseC()
 
-    # Affichage graphique du stocks de piÃ¨ces finies au sein de l'entrepÃ´t
+    # Affichage graphique du stocks des résultats au sein de l'entrepôt
     def affichageGraphique(self,compteurA,compteurB,compteurC,taille_max):
-        #CrÃƒÂ©ation de la fenÃªtre "resume_entrepot"
+        #CrÃ©ation de la fenêtre "resume_entrepot"
         resume_entrepot = Tk()
-        resume_entrepot.title("EntrepÃ´t")
-        #SpÃ©cification de la fenÃªtre, et sÃ©paration en 3 labels distincts pour sÃ©parer les 3 types de piÃ¨ces
+        resume_entrepot.title("Entrepôt")
+        #Spécification de la fenêtre, et séparation en 3 labels distincts pour séparer les 3 types de pièces
         p = PanedWindow(resume_entrepot, orient=HORIZONTAL)
         p.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
-        # Label des piÃ¨ces A
-        p.add(Label(p, text="\n Le nombre de piÃ¨ces de type A qui ont Ã©tÃ© traitÃ©es est de : " + str(compteurA) + "\n \nTaux de remplissage : " + str((compteurA/taille_max)*100) + "%\n", background='blue', anchor=CENTER))
-        # Label des piÃ¨ces B
-        p.add(Label(p, text="\n Le nombre de piÃ¨ces de type B qui ont Ã©tÃ© traitÃ©es est de : " + str(compteurB) + "\n \nTaux de remplissage : " + str((compteurB/taille_max)*100) + "%\n", background='red', anchor=CENTER))
-        # Label des piÃ¨ces C
-        p.add(Label(p, text="\n Le nombre de piÃ¨ces de type C qui ont Ã©tÃ© traitÃ©es est de : " + str(compteurC) + "\n \nTaux de remplissage : " + str((compteurC/taille_max)*100) + "%\n", background='lightgreen', anchor=CENTER))
+        # Label des pièces A
+        p.add(Label(p, text="\n Le nombre de pièces de type A qui ont été traitées est de : " + str(compteurA) + "\n \nTaux de remplissage : " + str((compteurA/taille_max)*100) + "%\n", background='blue', anchor=CENTER))
+        # Label des pièces B
+        p.add(Label(p, text="\n Le nombre de pièces de type B qui ont été traitées est de : " + str(compteurB) + "\n \nTaux de remplissage : " + str((compteurB/taille_max)*100) + "%\n", background='red', anchor=CENTER))
+        # Label des pièces C
+        p.add(Label(p, text="\n Le nombre de pièces de type C qui ont été traitées est de : " + str(compteurC) + "\n \nTaux de remplissage : " + str((compteurC/taille_max)*100) + "%\n", background='lightgreen', anchor=CENTER))
         p.pack()
     
-# Construction de l'entrepÃ´t nommÃ© "entrepot", et qui comporte comme seul paramÃ¨tre sa taille (calculÃ©e auparavent, le 10 n'est qu'un exemple).
+# Construction de l'entrepôt nommé "entrepot", et qui comporte comme seul paramètre sa taille (calculée auparavent, le 10 n'est qu'un exemple).
 entrepot = Entrepot(10);
 comptA=0;
 comptB=0;
 comptC=0;
-# Appel Ã  la fonction de rÃ©ception d'une piÃ¨ce
-# entrepot.recuperationPiece(comptA,comptB,comptC)
-# Affichage d'une fiche rÃ©sumÃ© Ã  un moment donnÃ© :
+# Appel à la fonction de réception d'un résultat
+# entrepot.recuperationPiece(resultat,comptA,comptB,comptC)
+# Affichage d'une fiche résumé à un moment donné :
 entrepot.affichageGraphique(comptA,comptB,comptC,entrepot.tailleMax)
