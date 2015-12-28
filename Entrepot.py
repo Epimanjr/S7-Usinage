b6cda8e946dadc4c3dcebab5524dccab8d113428
miage-thibaut <thibaut.rousselle7@gmail.com>
2015-12-28 23:32:59 +0100

    corrige la partie "Entrepôt"

--- Entrepot.py
+++ Entrepot.py
@@ -4,6 +4,8 @@ import os
 import sys
 # import nécessaire à la création de l'interface graphique
 from tkinter import *
+import Message
+import time
 
 class Entrepot: #Création de l'objet Entrepôt
 
@@ -29,34 +31,41 @@ class Entrepot: #Création de l'objet Entrepôt
     # Méthode qui récupère les pièces traitées par les machines (qui ont été modifiées en résultats de type rA/rB/rC précedemment), et incrémente le compteur lié
     def actualiserInterface(self):
         global typ
-        if typ == 'rA':
-            entrepot.increaseA()
-        elif typ == 'rB':
-            entrepot.increaseB()
-        elif typ == 'rC':
-            entrepot.increaseC()
+        if (comptA+comptB+comptC>=entrepot.tailleMax):
+            print("Impossible d'insérer une pièce supplémentaire !")
+        elif typ=='-2':
+            print('Fin de la file de messages')
+            fenetreEntrepot.destroy
         else:
-            print('Pièce qui non reconnue')
-        Texte.set("\n Le nombre de pièces de type A qui ont été traitées est de : " + str(comptA) + "\n \nTaux de remplissage : " + str((comptA/entrepot.tailleMax)*100) + "%\n")
-        Texte2.set("\n Le nombre de pièces de type B qui ont été traitées est de : " + str(comptB) + "\n \nTaux de remplissage : " + str((comptB/entrepot.tailleMax)*100) + "%\n")
-        Texte3.set("\n Le nombre de pièces de type C qui ont été traitées est de : " + str(comptC) + "\n \nTaux de remplissage : " + str((comptC/entrepot.tailleMax)*100) + "%\n")
-        
+            if typ == 'rA':
+                entrepot.increaseA()
+            elif typ == 'rB':
+                entrepot.increaseB()
+            elif typ == 'rC':
+                entrepot.increaseC()
+            else:
+                print('Pièce qui non reconnue')
+            Texte.set("\n Le nombre de pièces de type A qui ont été traitées est de : " + str(comptA) + "\n \nTaux de remplissage : " + str((comptA/entrepot.tailleMax)*100) + "%\n")
+            Texte2.set("\n Le nombre de pièces de type B qui ont été traitées est de : " + str(comptB) + "\n \nTaux de remplissage : " + str((comptB/entrepot.tailleMax)*100) + "%\n")
+            Texte3.set("\n Le nombre de pièces de type C qui ont été traitées est de : " + str(comptC) + "\n \nTaux de remplissage : " + str((comptC/entrepot.tailleMax)*100) + "%\n")
+            
     def initialisationInterface(self):
         Texte.set("\n Le nombre de pièces de type A qui ont été traitées est de : " + str(comptA) + "\n \nTaux de remplissage : " + str((comptA/entrepot.tailleMax)*100) + "%\n")
         Texte2.set("\n Le nombre de pièces de type B qui ont été traitées est de : " + str(comptB) + "\n \nTaux de remplissage : " + str((comptB/entrepot.tailleMax)*100) + "%\n")
         Texte3.set("\n Le nombre de pièces de type C qui ont été traitées est de : " + str(comptC) + "\n \nTaux de remplissage : " + str((comptC/entrepot.tailleMax)*100) + "%\n")
 
-
-# ----- MAIN PROGRAM ----- #
+# ----- MAIN PROGRAM TEST ----- #
 
 # Construction de l'entrepôt nommé "entrepot", et qui comporte comme seul paramètre sa taille (calculée auparavent, le 10 n'est qu'un exemple).
-entrepot = Entrepot(10);
+entrepot = Entrepot(5)
+
+#Initialisation des 3 compteurs pour les résultats stockés.
 comptA=0;
 comptB=0;
 comptC=0;
-#type choisi dans le compteur
-typ="rA"
 
+filetyp=["rB","rB","rA","rC","rA","-2"]
+    
 # Création de la fenêtre principale (main window)
 fenetreEntrepot = Tk()
 fenetreEntrepot.title('Entrepôt')
@@ -70,9 +79,11 @@ BoutonLancer.pack(side = BOTTOM, padx = 5, pady = 5)
 BoutonQuitter = Button(fenetreEntrepot, text ='Quitter', command = fenetreEntrepot.destroy)
 BoutonQuitter.pack(side = BOTTOM, padx = 5, pady = 5)
 
+#Initialisation des 3 textes pour les résultats stockés
 Texte = StringVar()
 Texte2 = StringVar()
 Texte3 = StringVar()
+    
 entrepot.initialisationInterface()
 
 #Spécification de la fenêtre, et séparation en 3 labels distincts pour séparer les 3 types de pièces
@@ -85,4 +96,14 @@ p.add(Label(p, textvariable=Texte2, background='red', anchor=CENTER))
 # Label des pièces C
 p.add(Label(p, textvariable=Texte3, background='lightgreen', anchor=CENTER))
 
-Mafenetre.mainloop()
+for typ in filetyp:
+    entrepot.actualiserInterface()
+    # Label des pièces A
+    p.update()
+    # Label des pièces B
+    p.update()
+    # Label des pièces C
+    p.update()
+    time.sleep(0.5)
+
+fenetreEntrepot.mainloop()
